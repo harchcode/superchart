20dbb70e2446e1ba3e886a2b596572590795b230
hjwlpjr <huang.zinwei@gmail.com>
2019-10-16 23:06:34 +0700

    add invert linear function

--- src/linear-scale.ts
+++ src/linear-scale.ts
@@ -10,3 +10,12 @@ export function linearScale(
   return (val: number): number =>
     ((val - minValue) / (maxValue - minValue)) * width;
 }
+
+export function invertLinearScale(
+  minValue: number,
+  maxValue: number,
+  width = 1
+): LinearScaleFunc {
+  return (val: number): number => 
+    (((val * maxValue) - (val * minValue)) + (minValue * width)) / width
+}
--- test/linear-scale.test.ts
+++ test/linear-scale.test.ts
@@ -1,5 +1,5 @@
 import test from 'ava';
-import { linearScale } from '../src/linear-scale';
+import { linearScale, invertLinearScale } from '../src/linear-scale';
 
 test('linear scale returns correct scale with default width', t => {
   const ls = linearScale(0, 100);
@@ -23,4 +23,12 @@ test('linear scale returns correct scale with specified width', t => {
   t.is(v, 500);
   t.is(v2, 750);
   t.is(v3, 0);
+});
+
+test('invert linear scale returns correct value with specified width', t => {
+  const ls = invertLinearScale(0, 100, 1000);
+
+  const v = ls(500);
+
+  t.is(v, 50);
 });
